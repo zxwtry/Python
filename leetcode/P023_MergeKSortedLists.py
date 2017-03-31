99b01243ddfff55485c6f20f9e7ea99f1e4c2e37
zxwtry <zxwtry@qq.com>
2017-03-31 21:59:31 +0800

    add leetcode/P023_MergeKSortedLists.py

--- leetcode/P023_MergeKSortedLists.py
+++ leetcode/P023_MergeKSortedLists.py
@@ -5,10 +5,10 @@
     @author:     zxwtry
     @email:      zxwtry@qq.com
     @date:       2017年3月29日
-    @details:    Solution: 56ms 52.31%
+    @details:    Solution: 302ms 11.23%
 '''
 
-from tools.Utils import *
+from leetcode.Utils import *
 
 class Solution(object):
     def cmp(self, a, b):
@@ -18,6 +18,11 @@ class Solution(object):
             return -1
         return a.val - b.val
     
+    def swap(self, a, i, j):
+        t = a[i]
+        a[i] = a[j]
+        a[j] = t
+    
     def mergeKLists(self, ls):
         """
         :type lists: List[ListNode]
@@ -25,10 +30,55 @@ class Solution(object):
         """
         lsn = 0 if ls == None else len(ls)
         if (lsn == 0): return []
-        heap, a, t = [], None, None
+        heap, a, hi = [None] * lsn, None, 0
         for i in range(lsn):
-            heap.append(i)
-        
+            if ls[i] != None:
+                heap[hi] = ls[i]
+                hi += 1
+        if hi == 0: return a
+        for i in range((lsn - 1) // 2, -1, -1):
+            p = i
+            c = 2 * p + 1
+            while c < hi:
+                if c + 1 < hi and self.cmp(heap[c + 1], heap[c]) < 0:
+                    c += 1
+                if self.cmp(heap[p], heap[c]) > 0:
+                    self.swap(heap, p, c)
+                else: break
+                p = c
+                c = 2 * p + 1
+        head = ListNode(0)
+        travel = head
+        while heap[0] != None:
+            temp = ListNode(heap[0].val)
+            travel.next = temp
+            travel = temp
+            if heap[0].next == None:
+                heap[0] = heap[hi - 1]
+                hi -= 1
+                if hi == 0: break
+            else:
+                heap[0].val = heap[0].next.val
+                heap[0].next = heap[0].next.next
+            p = 0
+            c = 2 * p + 1
+            while c < hi:
+                if c + 1 < hi and self.cmp(heap[c + 1], heap[c]) < 0:
+                    c += 1
+                if self.cmp(heap[p], heap[c]) > 0:
+                    self.swap(heap, p, c)
+                else: break
+                p = c
+                c = 2 * p + 1
+        return head.next
+    
+if __name__ == "__main__":
+    ls = [convertArrayToListNode([1, 4, 9, 13]), 
+          convertArrayToListNode([2, 6, 10, 14]),
+          convertArrayToListNode([3, 7, 11, 15]),
+          convertArrayToListNode([4, 8, 12, 16])]
+    a = Solution().mergeKLists(ls)
+    printListNode(a)
         
         
         
