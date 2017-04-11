599124101dc4f10a6cde00c4398d2c9f16833f64
zxwtry <zxwtry@qq.com>
2017-04-11 22:21:20 +0800

    add Python算法教程/P084_最大排列问题.py

--- Python算法教程/P084_最大排列问题.py
+++ Python算法教程/P084_最大排列问题.py
@@ -19,8 +19,34 @@ def native_max_perm(M, A=None):
         return native_max_perm(M, A)
     return A
 
+def loop_max_perm(M):
+    n = 0 if M == None else len(M)
+    A, count = set(range(n)), [0]*n
+    for i in M: count[i] += 1
+    Q = [i for i in A if count[i] == 0]
+    while Q:
+        i = Q.pop()
+        A.remove(i)
+        j = M[i]
+        count[j] -= 1
+        if count[j] == 0:
+            Q.append(j)
+    return A
+    
+from _collections import defaultdict
+
+def counting_sort(A, key=lambda x:x):
+    B, C = [], defaultdict(list)
+    for x in A:
+        C[key(x)].append(x)
+    for k in range(min(C), max(C)+1):
+        B.extend(C[k])
+    return B
+
 if __name__ == "__main__":
     M = [9, 0, 8, 1, 7, 2, 6, 3, 5, 4]
     M = [2, 2, 0, 5, 3, 5, 7, 4]
     A = native_max_perm(M, None)
+    B = counting_sort(M, None)
+    print(B)
     print(A)
